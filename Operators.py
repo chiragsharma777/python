441128fe9121a23a9421004243f727a09d887e3c
Chirag Sharma <chiragsharma352467@gmail.com>
2026-07-28 19:25:28 +0530

    first commit

---
+++ Operators.py
@@ -0,0 +1,24 @@
+# Arithmetic
+
+a = 10
+b =15
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+# Comparison
+
+print(a>b)
+print(a<b)
+print(a==b)
+
+# Logical
+
+print(True and False)
+print(True or False)
+print(not True)
