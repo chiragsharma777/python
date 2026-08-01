ae36915661f02155be4d5fd43a5d485e88018e70
Chirag Sharma <chiragsharma352467@gmail.com>
2026-08-01 21:21:39 +0530

    Add files via upload

---
+++ If.py
@@ -0,0 +1,9 @@
+# age = int(input("Enter your agr: "))
+
+# if age >= 18:
+#     print(f"Your age is {age}")
+# elif age <0:
+#     print("Invalid age")
+# else:
+#     print("You are underlimit")
+
