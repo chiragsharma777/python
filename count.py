02266bd46ba6cb8604d67537b1bea700a9ee73f7
Chirag Sharma <chiragsharma352467@gmail.com>
2026-08-24 20:19:10 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,14 @@
+numbers = [10, 20, 10, 30, 20, 10, 40, 30, 20, 50]
+
+frequency = {}
+
+for num in numbers:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num] = 1
+
+print("Element Frequency:")
+
+for key, value in frequency.items():
+    print(key, "->", value)
