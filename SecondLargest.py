d396cb33ee79371cf310eb67d2104dec058660db
Chirag Sharma <chiragsharma352467@gmail.com>
2026-09-09 18:34:25 +0530

    Add files via upload

---
+++ SecondLargest.py
@@ -0,0 +1,14 @@
+numbers = [10, 25, 5, 40, 30, 40]
+
+largest = numbers[0]
+second_largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        second_largest = largest
+        largest = num
+    elif num > second_largest and num != largest:
+        second_largest = num
+
+print("Largest:", largest)
+print("Second Largest:", second_largest)
